9ba440e06feea3936c76970564b7463c00aa5a38
Cesar <cesar.decasasgarcia@gmail.com>
2020-10-19 09:40:23 -0700

    pokemon name, img and stats displaying

--- script.js
+++ script.js
@@ -3,13 +3,68 @@
 const input = document.querySelector('#input')
 const button = document.querySelector('#button')
 const domain = 'https://pokeapi.co/api/v2/pokemon'
+const pokemonPlace = document.querySelector('.pokemon') 
 
-const getPokemon = () => {
+
+const rederPokemon = poke =>{
+        let pokemonH = document.createElement('h3')
+        pokemonH.innerHTML = `${poke.name} #${poke.id}`
+        pokemonH.classList.add ('pokeName')
+        pokemonPlace.append(pokemonH)
+
+        let pokeDiv = document.createElement('div')
+        pokemonPlace.append(pokeDiv)
+
+        let pokeImg = document.createElement('img')
+        pokeImg.src = poke.sprites.front_default
+        pokeImg.classList.add ('pImg')
+        pokeDiv.append(pokeImg)
+
+        let statsList = document.createElement('ul')
+        pokeDiv.append(statsList)
+
+        let hpstat = document.createElement('li')
+        hpstat.innerHTML = `${poke.stats[0].stat.name}:${poke.stats[0].base_stat}`
+        statsList.appendChild(hpstat)
+
+        let attstat = document.createElement('li')
+        attstat.innerHTML = `${poke.stats[1].stat.name}:${poke.stats[1].base_stat}`
+        statsList.appendChild(attstat)
+
+        let defstat = document.createElement('li')
+        defstat.innerHTML = `${poke.stats[2].stat.name}:${poke.stats[2].base_stat}`
+        statsList.appendChild(defstat)
+
+        let spastat = document.createElement('li')
+        spastat.innerHTML = `${poke.stats[3].stat.name}:${poke.stats[3].base_stat}`
+        statsList.appendChild(spastat)
+
+        let spdstat = document.createElement('li')
+        spdstat.innerHTML = `${poke.stats[4].stat.name}:${poke.stats[4].base_stat}`
+        statsList.appendChild(spdstat)
+
+        let spestat = document.createElement('li')
+        spestat.innerHTML = `${poke.stats[5].stat.name}:${poke.stats[5].base_stat}`
+        statsList.appendChild(spestat)
+
+        // poke.stats.forEach(() =>{
+        //     let bstat = document.createElement('li')
+        //     bstat.innerHTML = `${poke.stats.stat.name}:${base_stat}`
+        //     statsList.appendChild(bstat)
+        // })
+}
+
+
+const getPokemon = async () => {
+    try{
     const search = `${domain}/${input.value}/`
-    axios.get(search)
-    .then(response => {
-        console.log(response)
-    })
+    const response = await axios.get(search)
+    
+        console.log(response.data)
+        rederPokemon(response.data)
+    }catch(error){
+        console.log(error)
+    }
 }
 
 button.addEventListener('click', () =>{
