1c691b756c05650ea026391a8b5ff91d0c1e8b7d
Cesar <cesar.decasasgarcia@gmail.com>
2020-10-20 07:24:50 -0700

    search pokemon by type done

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const input = document.querySelector('#input')
 const button = document.querySelector('#button')
 const domain = 'https://pokeapi.co/api/v2/pokemon'
 const pokemonPlace = document.querySelector('.pokemon') 
-const characteristics = 'https://pokeapi.co/api/v2/characteristic/'
+const pokemonPlace2 = document.querySelector('.pokemon2')
+const pokemonPlace3 = document.querySelector('.pokemon3')
+const characteristics = 'https://pokeapi.co/api/v2/pokemon-species/'
+const types = 'https://pokeapi.co/api/v2/type/'
 
 
 const renderPokemon = poke =>{
@@ -23,7 +26,6 @@ const renderPokemon = poke =>{
             pokeDiv.appendChild(pokemonType)
         });
 
-
         let pokeImg = document.createElement('img')
         pokeImg.src = poke.sprites.front_default
         pokeImg.classList.add ('pImg')
@@ -33,7 +35,6 @@ const renderPokemon = poke =>{
         pokeDiv.append(statsList)
 
         poke.stats.forEach(stat =>{
-            console.log(stat)
             let bstat = document.createElement('li')
             bstat.innerHTML = `${stat.stat.name}:${stat.base_stat}`
             statsList.appendChild(bstat)
@@ -42,13 +43,25 @@ const renderPokemon = poke =>{
 }
 const renderCharacteristics = data => {
         let pokeDiv = document.createElement('div')
-        pokemonPlace.appendChild(pokeDiv)
+        pokemonPlace2.appendChild(pokeDiv)
 
         let pokeC = document.createElement('p')
-        pokeC.innerHTML = data.descriptions[2].description
+        pokeC.innerHTML = data.flavor_text_entries[0].flavor_text
         pokeDiv.appendChild(pokeC)
 }
 
+const renderTypeList = data => {
+    let pokeDiv = document.createElement('div')
+    pokemonPlace.appendChild(pokeDiv)
+
+    data.pokemon.forEach(poke =>{
+        let name = document.createElement('h3')
+        name.classList.add('pokelist')
+        name.innerHTML = `${poke.pokemon.name}`
+        pokeDiv.appendChild(name)
+
+    })
+}
 
 const getPokemon = async () => {
     try{
@@ -62,23 +75,37 @@ const getPokemon = async () => {
     }
 }
 
-// const getCharacteristics = async () =>{
-//     try{
-//         const searchC = `${characteristics}${input.value}/`
-//         const response = await axios.get(searchC)
-//         console.log(response)
-//         renderCharacteristics(response.data)
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
+const getCharacteristics = async () =>{
+    try{
+        const searchC = `${characteristics}${input.value}/`
+        const response = await axios.get(searchC)
+        console.log(response.data)
+        renderCharacteristics(response.data)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const getTypeList = async () =>{
+    try{
+        const search = `${types}${input.value}/`
+        const response = await axios.get(search)
+        console.log(response.data)
+        renderTypeList(response.data)
+    }catch(error){
+        console.log(error)
+    }
+} 
 
 const rmPoke = () => {
-//  pokemonPlace.removeChild(pokemonPlace.firstChild)
+    if(pokemonPlace.lastChild){
  pokemonPlace.removeChild(pokemonPlace.lastChild)
-//  pokemonPlace.removeChild(pokemonPlace.lastChild)
-
+    }
+    if(pokemonPlace2.lastChild){
+        pokemonPlace2.removeChild(pokemonPlace2.lastChild)
+        }
 }
+
 
 button.addEventListener('click', () =>{
     console.log(input.value)
@@ -87,5 +114,6 @@ button.addEventListener('click', () =>{
     }
     rmPoke()
     getPokemon()
-    // getCharacteristics()
+    getCharacteristics()
+    getTypeList()
 })
